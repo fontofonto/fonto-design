9ba8252bca65fcb6a99e506c61b4c46e5c5f96bd
fontofonto <donlam1994@gmail.com>
2022-01-16 05:33:41 -0800

    add words, basic layout and styles, test svg background

---
+++ components/LinesBackground.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg class="bg-lines">
+    <line
+      class="block md:hidden"
+      v-for="index in 5"
+      :key="`bgline${index}`"
+      :x1="`${(100 / 5) * index}%`"
+      y1="0%"
+      :x2="`${(100 / 5) * index}%`"
+      y2="100%"
+      stroke="#DEDBD0"
+    />
+    <line
+      class="hidden md:block"
+      v-for="index in 13"
+      :key="`bgline${index}`"
+      :x1="`${(100 / 13) * index}%`"
+      y1="0%"
+      :x2="`${(100 / 13) * index}%`"
+      y2="100%"
+      stroke="#DEDBD0"
+    />
+  </svg>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="postcss" scoped>
+.bg-lines {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -99;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    <h1 class="text-9xl font-bold">FONTO</h1>
-    <h1 class="text-5xl font-medium">User Experience Designer</h1>
+  <div class="page-wrapper">
+    <!-- intro -->
+    <div class="intro-wrapper">
+      <h1 class="title">FONTO</h1>
+      <h2 class="subtitle">
+        An <span>User Experience Designer</span> and
+        <span>Frontend Developer</span> from Hong Kong. <br /><br />Now living
+        in Vancouver.
+      </h2>
+    </div>
+    <!-- resume -->
+    <div class="resume-wrapper">
+      <a href="#!">Get My Resume</a>
+    </div>
+    <!-- Work -->
+    <div class="work-wrapper">
+      <h3 class="title">Work</h3>
+      <a href="#!">RewardMe</a>
+      <a href="#!">MeFi</a>
+      <a href="#!">Measurable Data Tokens</a>
+    </div>
+    <LinesBackground />
   </div>
 </template>
 
@@ -10,3 +29,38 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style lang="postcss" scoped>
+.page-wrapper {
+  @apply px-20;
+}
+.intro-wrapper {
+  @apply mt-10;
+  .title {
+    @apply text-9xl font-bold;
+  }
+  .subtitle {
+    @apply text-4xl font-medium mt-10;
+    line-height: 150%;
+    span {
+      @apply relative;
+      &:after {
+        @apply block absolute -z-[1] w-[102%] h-4 bottom-0 left-1/2 transform -translate-x-1/2 bg-yellow-500 rounded-sm;
+        content: '';
+      }
+    }
+  }
+}
+.resume-wrapper {
+  @apply mt-20 text-4xl font-bold text-right;
+}
+.work-wrapper {
+  @apply mt-40;
+  .title {
+    @apply font-light italic text-5xl;
+  }
+  a {
+    @apply block font-medium text-7xl mt-10;
+  }
+}
+</style>
